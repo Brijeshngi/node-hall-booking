f4d0e8ed071b159b3f3706e1f2923ed702e90b5e
Brijesh Gupta <brijeshngi@gmail.com>
2023-05-17 18:09:26 +0530

    modified

--- config/database.js
+++ config/database.js
@@ -3,5 +3,5 @@ import mongoose from "mongoose";
 export const connectMongoDBAtlas = async () => {
   const { connection } = await mongoose.connect(process.env.MONGODB_URI);
   console.log(`Connected to MongoDB Atlas`);
-  //   console.log(connection);
+  // console.log(connection);
 };
